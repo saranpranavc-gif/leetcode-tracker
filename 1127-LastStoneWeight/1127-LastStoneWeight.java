c57689f39380672a84f08403168e9ca4c4944d36
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:48:03 +0530

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.java
@@ -0,0 +1,25 @@
+// Last updated: 09/07/2026, 09:47:36
+class Solution {
+
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+
+        while (pq.size() > 1) {
+
+            int first = pq.poll();
+            int second = pq.poll();
+
+            if (first != second) {
+                pq.add(first - second);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
