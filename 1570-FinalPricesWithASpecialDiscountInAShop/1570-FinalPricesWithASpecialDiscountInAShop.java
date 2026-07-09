f2678bb0900cd195ff5cd122aead567ff9f67176
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:47:29 +0530

    Create 1570-FinalPricesWithASpecialDiscountInAShop

---
+++ 1570-FinalPricesWithASpecialDiscountInAShop/1570-FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:47:02
+class Solution {
+    public int[] finalPrices(int[] prices) {
+
+        int n = prices.length;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+
+        return prices;
+    }
+}
