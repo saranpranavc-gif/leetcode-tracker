6536e4e648c9ebb5e161edc0150d20745a2d0322
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:47:20 +0530

    Create 1626-CanMakeArithmeticProgressionFromSequence

---
+++ 1626-CanMakeArithmeticProgressionFromSequence/1626-CanMakeArithmeticProgressionFromSequence.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:46:52
+import java.util.*;
+
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+
+        Arrays.sort(arr);
+
+        int diff = arr[1] - arr[0];
+
+        for (int i = 2; i < arr.length; i++) {
+
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
