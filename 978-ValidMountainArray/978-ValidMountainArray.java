c9c793149a5d8d556ccbc70e9faee339c55dd207
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:48:22 +0530

    Create 978-ValidMountainArray

---
+++ 978-ValidMountainArray/978-ValidMountainArray.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 09:47:55
+class Solution {
+
+    public boolean validMountainArray(int[] arr) {
+
+        int n = arr.length;
+        int i = 0;
+
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        if (i == 0 || i == n - 1) {
+            return false;
+        }
+
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+}
