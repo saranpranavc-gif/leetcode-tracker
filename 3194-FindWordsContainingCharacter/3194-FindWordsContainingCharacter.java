9325fdd064873672737bd0a38297ae2b5f570bdd
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:46:28 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 09:46:00
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
