6cb1abaea75036c79e6c93fce1e5198a3ab51df0
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:50:14 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 09:49:46
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == xcopy;        
+    }
+}
