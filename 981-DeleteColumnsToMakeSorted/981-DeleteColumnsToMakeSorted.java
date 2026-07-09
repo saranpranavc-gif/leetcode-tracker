0994372ee746eb6eb5f964c956a177365b403af8
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:48:20 +0530

    Create 981-DeleteColumnsToMakeSorted

---
+++ 981-DeleteColumnsToMakeSorted/981-DeleteColumnsToMakeSorted.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 09:47:53
+class Solution {
+    public int minDeletionSize(String[] strs) {
+
+        int count = 0;
+
+        for (int col = 0; col < strs[0].length(); col++) {
+
+            for (int row = 1; row < strs.length; row++) {
+
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+
+                    count++;
+                    break;
+
+                }
+            }
+        }
+
+        return count;
+    }
+}
