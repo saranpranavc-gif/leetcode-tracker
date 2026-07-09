7266510d31f711823757e0ec4c8772e87a512019
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:48:23 +0530

    Create 965-UniqueEmailAddresses

---
+++ 965-UniqueEmailAddresses/965-UniqueEmailAddresses.java
@@ -0,0 +1,28 @@
+// Last updated: 09/07/2026, 09:47:56
+class Solution {
+
+    public int numUniqueEmails(String[] emails) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (String email : emails) {
+
+            String[] parts = email.split("@");
+
+            String local = parts[0];
+            String domain = parts[1];
+
+            int plusIndex = local.indexOf('+');
+
+            if (plusIndex != -1) {
+                local = local.substring(0, plusIndex);
+            }
+
+            local = local.replace(".", "");
+
+            set.add(local + "@" + domain);
+        }
+
+        return set.size();
+    }
+}
