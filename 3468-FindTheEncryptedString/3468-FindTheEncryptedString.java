6f9733b676a84fa91511dc4b6734bc1316143126
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:46:15 +0530

    Create 3468-FindTheEncryptedString

---
+++ 3468-FindTheEncryptedString/3468-FindTheEncryptedString.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 09:45:47
+class Solution {
+    public String getEncryptedString(String s, int k) {
+
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append(s.charAt((i + k) % n));
+        }
+
+        return sb.toString();
+    }
+}
