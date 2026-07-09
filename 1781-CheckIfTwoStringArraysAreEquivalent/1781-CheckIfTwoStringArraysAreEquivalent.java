f46a0d57145ff9f74ed927b1486a5854bd9733d9
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:46:51 +0530

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:46:24
+class Solution {
+
+    public boolean arrayStringsAreEqual(String[] word1,
+                                        String[] word2) {
+
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        for (String str : word1) {
+            s1.append(str);
+        }
+
+        for (String str : word2) {
+            s2.append(str);
+        }
+
+        return s1.toString().equals(s2.toString());
+    }
+}
