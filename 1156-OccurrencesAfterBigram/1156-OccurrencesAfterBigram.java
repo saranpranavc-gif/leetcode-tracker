bf5496b4c04c141fe65f087f6771efec4137b22e
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:47:58 +0530

    Create 1156-OccurrencesAfterBigram

---
+++ 1156-OccurrencesAfterBigram/1156-OccurrencesAfterBigram.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:47:30
+class Solution {
+
+    public String[] findOcurrences(String text, String first, String second) {
+
+        String[] words = text.split(" ");
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length - 2; i++) {
+
+            if (words[i].equals(first) &&
+                words[i + 1].equals(second)) {
+
+                result.add(words[i + 2]);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
