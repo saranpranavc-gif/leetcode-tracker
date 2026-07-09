fcbc83d543d4915b9e4ab6de820335c17ec1903e
saranpranavc-gif <saranpranav.c@gmail.com>
2026-07-09 09:46:26 +0530

    Create 3421-CountPairsThatFormACompleteDayI

---
+++ 3421-CountPairsThatFormACompleteDayI/3421-CountPairsThatFormACompleteDayI.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 09:45:59
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+
+        int count = 0;
+
+        for (int i = 0; i < hours.length; i++) {
+            for (int j = i + 1; j < hours.length; j++) {
+
+                if ((hours[i] + hours[j]) % 24 == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
